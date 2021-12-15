1c7dba901bfa659e81ecf8b06a716b55f90f8e7c
ErikCeron1 <erikceron@unicauca.edu.co>
2021-12-15 15:26:14 -0500

    Nuevo farmacia ceron
    Se suben modificaciones farmacia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
