789dc65b8a234e6caba00e1d2eeb544d6ec1cd29
ErikCeron1 <erikceron@unicauca.edu.co>
2021-12-23 08:16:17 -0500

    CrearDosidicacionFull
    Se suben cambios de editar dosificacion y creacion de login

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -21,7 +21,7 @@ export class HeaderComponent implements OnInit, AfterViewInit {
   }
 
   logOut(){
-
+    alert("cerrando sesion");
   }
   
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,27 +2,44 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatGridListModule } from '@angular/material/grid-list';
-import {MatIconModule} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from "@angular/material/input";
+import { LoginComponent } from './services/login/login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     MatGridListModule,
     MatIconModule,
     MatTabsModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule
   ],
   exports: [
     MatGridListModule,
     MatIconModule,
     MatTabsModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatDialogModule,
+    FormsModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    MatInputModule
   ]
 })
 export class SharedModule { }
